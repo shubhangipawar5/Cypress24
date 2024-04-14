abd18d927f44d7898dcd164a580baa26167d18e1
shubhangipawar5 <shubhangi5693@gmai.com>
2024-04-14 08:42:48 +0530

    chnages of github action

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-
+  projectId: "4pzs8b",
 
   e2e: {
     baseUrl: "https://rahulshettyacademy.com/AutomationPractice/",
     testIsolation: false,
     failOnStatusCode: false,
+    screenshotsFolder: 'cypress/screenshots',
+  
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/support/util1.js
+++ cypress/support/util1.js
@@ -8,5 +8,10 @@ class diffurl{
             return "https://ultimateqa.com/automation"
         }
     }
+     generateProjectId() {
+        const timestamp = Date.now();
+        const randomValue = Math.floor(Math.random() * 1000); // Adjust the range as needed
+        return `project_${timestamp}_${randomValue}`;
+    }
 }
 export let difurl = new diffurl();
